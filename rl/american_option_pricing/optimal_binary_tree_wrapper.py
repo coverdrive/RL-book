9fcdf8377654b34cc7ba936f7e4bc6c90cfab455
vishalmohanty <vishalmohanty97@gmail.com>
2022-12-06 23:12:17 -0800

    Add optimal binary tree wrapper

--- rl/american_option_pricing/optimal_binary_tree_wrapper.py
+++ rl/american_option_pricing/optimal_binary_tree_wrapper.py
@@ -1,10 +1,15 @@
+import numpy as np
 import sys
-from typing import Callable, List, Sequence, Tuple
-sys.path.append("../../")
+from typing import Callable, Tuple, Iterator, List
+sys.path.append("../", "../../")
 
 from american_option_pricing import AlgoWrapper
 from price_simulator import SimulationPath
-from rl.chapter8.optimal_exercise_bin_tree import OptimalExerciseBinTree
+from dynamic_programming import V
+from markov_decision_process import Terminal, NonTerminal
+from policy import FiniteDeterministicPolicy
+from distribution import Constant, Categorical
+from finite_horizon import optimal_vf_and_policy
 
 class OptimalBinaryTreeWrapper(AlgoWrapper):
     def __init__(
@@ -26,17 +31,55 @@ class OptimalBinaryTreeWrapper(AlgoWrapper):
             payoff_func = payoff_func
         )
         self.num_steps = 1000
-        self.opt_ex_bin_tree: OptimalExerciseBinTree = OptimalExerciseBinTree(
-            spot_price=spot_price,
-            payoff=payoff_func,
-            expiry=expiry,
-            rate=rate,
-            vol=vol,
-            num_steps=self.num_steps
+        self.vf_seq_best = None
+        self.policy_seq_best = None
+
+    def dt(self) -> float:
+        """
+        Get the time interval size used for discretization 
+        """
+        return self.expiry / self.num_steps
+
+    def state_price(self, i: int, j: int) -> float:
+        """
+        Get the price associated with state j of time step i.
+        """
+        return self.spot_price * np.exp((2 * j - i) * self.vol *
+                                        np.sqrt(self.dt()))
+
+    def get_opt_vf_and_policy(self) -> \
+            Iterator[Tuple[V[int], FiniteDeterministicPolicy[int, bool]]]:
+        """
+        Compute the optimal value functions and optimal policies for each time step by backward induction.
+        """
+        dt: float = self.dt()
+        up_factor: float = np.exp(self.vol * np.sqrt(dt))
+        up_prob: float = (np.exp(self.rate * dt) * up_factor - 1) / \
+            (up_factor * up_factor - 1)
+        # this step calls this function from the finite_horizon module.
+        return optimal_vf_and_policy( 
+            steps=[
+                {NonTerminal(j): {
+                    True: Constant(
+                        (
+                            Terminal(-1),
+                            self.payoff(self.state_price(i, j))
+                        )
+                    ),
+                    False: Categorical(
+                        {
+                            (NonTerminal(j + 1), 0.): up_prob,
+                            (NonTerminal(j),     0.): 1 - up_prob
+                        }
+                    )
+                } for j in range(i + 1)}
+                for i in range(self.num_steps + 1)
+            ],
+            gamma=np.exp(-self.rate * dt)
         )
 
     def train(self, simulation_paths: List[SimulationPath] = None) -> None:
-        vf_seq, policy_seq = zip(self.opt_ex_bin_tree.get_opt_vf_and_policy())
-        ex_boundary: Sequence[Tuple[float, float]] = self.opt_ex_bin_tree.option_exercise_boundary(policy_seq, is_call=False)
+        self.vf_seq_best, self.policy_seq_best = zip(*self.get_opt_vf_and_policy())
 
     def predict(self, time_to_expiry: float, price: float) -> float:
+        raise NotImplementedError
